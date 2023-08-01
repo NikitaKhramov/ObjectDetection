362758c900d5e3805bed0deaeb50686eb26aeb0b
NikitaKhramov <61798948+NikitaKhramov@users.noreply.github.com>
2023-08-01 12:26:07 +0300

    build

---
+++ precache-manifest.cfe5e17b8169b50ba08fd4f039a194bb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3369dd7d71d0aef915da65aad7ed3782",
+    "url": "/index.html"
+  },
+  {
+    "revision": "99c6835dda407561dddf",
+    "url": "/static/css/main.d7aacf9f.chunk.css"
+  },
+  {
+    "revision": "c935fba206b9ee07fa86",
+    "url": "/static/js/2.7825e04f.chunk.js"
+  },
+  {
+    "revision": "b183437b48fb0c7c10973acfa520fbc1",
+    "url": "/static/js/2.7825e04f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "99c6835dda407561dddf",
+    "url": "/static/js/main.dc415aac.chunk.js"
+  },
+  {
+    "revision": "949eb168f1d833ae83d5",
+    "url": "/static/js/runtime-main.743d20cc.js"
+  }
+]);
